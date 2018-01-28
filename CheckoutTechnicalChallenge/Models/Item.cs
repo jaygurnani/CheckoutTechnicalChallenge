7a707c5bd8444b6efbc2a7cda5ec74998eabc819
Jay <jay.gurnani@gmail.com>
2018-01-28 18:47:05 +1100

    Initial Commit

---
+++ CheckoutTechnicalChallenge/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheckoutTechnicalChallenge.Models
+{
+    public class Item
+    {
+        public string ItemName { get; set; }
+        public string int ItemQuantity { get; set; }
+    }
+}
